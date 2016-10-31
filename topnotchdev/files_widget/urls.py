1587324dcf4ba8327ef81112f72db56190fae041
Brett MacDonald <ArtistsTechGuy@users.noreply.github.com>
2016-10-31 04:56:47 -0300

    Update urls.py
    minor fix #2

--- topnotchdev/files_widget/urls.py
+++ topnotchdev/files_widget/urls.py
@@ -6,6 +6,6 @@ except ImportError:
 from topnotchdev.files_widget.views import upload, thumbnail_url
 
 urlpatterns = [
-    url(u'^upload/$', "upload", name="files_widget_upload"),
-    url(u'^thumbnail-url/$', "thumbnail_url", name="files_widget_get_thumbnail_url"),
+    url(u'^upload/$', upload, name="files_widget_upload"),
+    url(u'^thumbnail-url/$', thumbnail_url, name="files_widget_get_thumbnail_url"),
 ]
